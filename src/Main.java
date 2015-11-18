402a8c87a79910cc46cb501c6d915b13c953249a
Geraldo <Geraldo@Neto>
2015-11-18 16:12:37 -0200

    #Questão 1 80% completa, faltando apenas alguns detalhes.

--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,18 @@ public class Main {
 	PCB aux = null;
 	int contador = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		
 		ArquivoUtils arq = new ArquivoUtils();
 		// ler 10 processos
 		LinkedList<PCB> saida = arq.ler_processos();
 		Main m = new Main();
 		m.preenche_fila_prontos(saida);
 		TiposEstados.exibe_PCB(m.prontos.get(0));
-		m.exe.start();
+		while(!m.prontos.isEmpty()){
+			new Thread(m.exe).start();
+			Thread.sleep(100);
+		}
 	}
 
 		
@@ -31,51 +34,28 @@ public class Main {
 	
 	Thread exe = new Thread() {
 		public void run() {
-			while (!prontos.isEmpty()) {
 				aux = prontos.get(0);
-				while (aux.getNumeroPicosCPU() > aux.getPicoCPUAtual()) {
-					aux.setEstado(TiposEstados.EXECUTANDO);
-					if (aux.getPicosCPU()[aux.getPicoCPUAtual()] > 10) {
-						int contador_interno = 0;
-						while (contador_interno < (aux.getPicosCPU()[aux
-								.getPicoCPUAtual()] - 10)) {
-							contador++;
-							contador_interno++;
-						}
-						aux.setEstado(TiposEstados.ESPERA);
-						espera.add(aux);
-						//prontos.remove(aux);
-						if (!espera.isEmpty()) {
-							new Thread(esperar).start();
+				aux.setEstado(TiposEstados.EXECUTANDO);
+				int contador_interno = 0;
+				while (contador_interno < aux.getPicosCPU()[aux.getPicoCPUAtual()]) {
+					contador++;
+					contador_interno++;
+				}
+				aux.setEstado(TiposEstados.ESPERA);
+				espera.add(aux);
+				prontos.remove(aux);
+					new Thread(esperar).start();
 							synchronized (esperar) {
 								try {
-									esperar.wait();
+									esperar.join();
 								} catch (InterruptedException e) {
 									// TODO Auto-generated catch block
 									e.printStackTrace();
 								}
 							}
-						}
 						System.out.println("contador interno "
 								+ contador_interno);
-
-					} else {
-						int contador_interno = 0;
-						while (contador_interno < (aux.getPicosCPU()[aux
-								.getPicoCPUAtual()])) {
-							contador++;
-							contador_interno++;
-						}
-						System.out.println("contador interno "
-								+ contador_interno);
-					}
-					int pico = aux.getPicoCPUAtual();
-					aux.setPicoCPUAtual(++pico);
-					aux.setContador(contador);
-					prontos.remove(aux);
-					System.out.println("contador " + aux.getContador());
-				}
-			}
+						System.out.println("Tempo de espera "+ contador);
 		}
 
 	};
@@ -83,12 +63,21 @@ public class Main {
 	Thread esperar = new Thread() {
 		public void run() {
 			synchronized (this) {
-
 				for (int i = 0; i < 10; i++) {
 					contador++;
 				}
-				espera.remove(aux);
-				this.notifyAll();
+				PCB obj = espera.get(0);
+				//prontos.add(obj);
+				int pico = obj.getPicoCPUAtual();
+				obj.setPicoCPUAtual(++pico);
+				if(obj.getPicoCPUAtual() >= obj.getNumeroPicosCPU()){
+					obj.setEstado(TiposEstados.FINALIZADO);
+					espera.remove(0);
+				}else{
+					prontos.add(obj);
+					espera.remove(0);
+				}
+				this.notify();
 			}
 		}
 	};
