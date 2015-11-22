23a4f4bad8a35016a4f9e5550e40d6526edd0edd
Geraldo <Geraldo@Neto>
2015-11-22 18:22:23 -0200

    #

--- src/TiposEstados.java
+++ src/TiposEstados.java
@@ -1,19 +1,18 @@
-
 public class TiposEstados {
 	static final public int PRONTO = 1;
 	static final public int EXECUTANDO = 2;
 	static final public int ESPERA = 3;
 	static final public int FINALIZADO = 4;
-	
-	static public void exibePCB(PCB pcb){
-		if(pcb.getEstado() == PRONTO){
+
+	static public void exibePCB(PCB pcb) {
+		if (pcb.getEstado() == PRONTO) {
 			System.out.println("Estado PRONTO");
-		}else if(pcb.getEstado() == EXECUTANDO){
+		} else if (pcb.getEstado() == EXECUTANDO) {
 			System.out.println("Estado EXECUTANDO");
-		}else{
+		} else {
 			System.out.println("Estado ESPERA");
 		}
-		System.out.println("PID "+ pcb.getId_processo());
+		System.out.println("PID " + pcb.getId_processo());
 		System.out.println("Número de picos CPU " + pcb.getNumeroPicosCPU());
 		System.out.println("Pico atual " + pcb.getPicoCPUAtual());
 	}
